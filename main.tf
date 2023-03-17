7e14f2cf2cad2c0cf43468878df9b03862e1c835
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-03-17 19:36:19 -0400

    simple case

--- main.tf
+++ main.tf
@@ -1,64 +1,45 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
+# Define provider configuration
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+  token   = "my-root-token"
+}
+
+resource "vault_mount" "kv-v1" {
+  path = "kv-v1"
+  type = "kv"
+  options = {
+    version = "1"
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-resource "docker_container" "vault" {
-  name  = "vault"
-  image = "vault"
-
-  env = [
-    "VAULT_DEV_ROOT_TOKEN_ID=root",
-    "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200"
-  ]
-
-  ports {
-    internal = 8200
-    external = 8200
+resource "vault_mount" "kv-v2" {
+  path = "kv-v2"
+  type = "kv"
+  options = {
+    version = "2"
   }
 }
 
-resource "null_resource" "wait_for_vault" {
-  depends_on = [docker_container.vault]
-
-  provisioner "local-exec" {
-    command = "vault login root"
-  }
-
-  provisioner "local-exec" {
-    command     = "vault secrets list"
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      VAULT_ADDR = "http://${docker_container.vault.hostname}:8200"
-    }
-  }
-}
-
-resource "vault_generic_secret" "my_secret" {
-  depends_on = [null_resource.wait_for_vault]
-
-  path = "secret/my_secret"
-
+resource "vault_generic_secret" "my_secret_v1" {
+  path = "${vault_mount.kv-v2.path}/secret"
   data_json = jsonencode({
-    username = "hello",
-    password = "pass",
+    password = "my-password-v1"
   })
 }
 
-output "secret_username" {
-  value     = "${vault_generic_secret.my_secret.data["username"]}"
+resource "vault_generic_secret" "my_secret_v2" {
+  path = "${vault_mount.kv-v2.path}/secret"
+  data_json = jsonencode({
+    password = "my-password-v2"
+  })
+}
+
+output "secret_password_v1" {
+  value     = "${vault_generic_secret.my_secret_v1.data["password"]}"
   sensitive = true
 }
 
-output "secret_password" {
-  value     = "${vault_generic_secret.my_secret.data["password"]}"
+output "secret_password_v2" {
+  value     = "${vault_generic_secret.my_secret_v2.data["password"]}"
   sensitive = true
 }
