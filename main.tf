3794a4bd5a3a84bde41f0d050d867f0dc4bdf5dc
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-03-17 20:09:51 -0400

    docker

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+    vault = {
+      source = "hashicorp/vault"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_container" "vault" {
+  name  = "vault"
+  image = "vault:1.13.0"
+  ports {
+    internal = 8200
+    external = 8200
+  }
+  env = [
+    "VAULT_DEV_ROOT_TOKEN_ID=my-root-token",
+    "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200",
+    "VAULT_ADDR=http://127.0.0.1:8200",
+  ]
+}
+
 provider "vault" {
   address = "http://127.0.0.1:8200"
   token   = "my-root-token"
@@ -20,7 +48,7 @@ resource "vault_mount" "kv-v2" {
 }
 
 resource "vault_generic_secret" "my_secret_v1" {
-  path = "${vault_mount.kv-v2.path}/secret"
+  path = "${vault_mount.kv-v1.path}/secret"
   data_json = jsonencode({
     password = "my-password-v1"
   })
